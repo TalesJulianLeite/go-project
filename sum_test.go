9649d615cb61a349b338ec1e20ae916b4f59cb2d
TalesJulianLeite <talesjulianl@gmail.com>
2025-07-08 00:05:35 -0300

    Criação de metodos e de testes unitarios

--- sum_test.go
+++ sum_test.go
@@ -2,10 +2,36 @@ package main
 
 import "testing"
 
-func TestSum(t *testing.T) {
-    result := sum(2, 3)
+const ERROR_MESSAGE = "The result must be "
 
-    if result != 5 {
-        t.Error("The result must be 5")
-    }
+func TestSum(t *testing.T) {
+	result := sum(2, 3)
+
+	if result != 5 {
+		t.Error(ERROR_MESSAGE + "5")
+	}
+}
+
+func TestSub(t *testing.T) {
+	result := sub(5, 2)
+
+	if result != 3 {
+		t.Error(ERROR_MESSAGE + "3")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := times(6, 2)
+
+	if result != 12 {
+		t.Error(ERROR_MESSAGE + "12")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := sumX(3, 2)
+
+	if result != 8 {
+		t.Error(ERROR_MESSAGE + "8")
+	}
 }
